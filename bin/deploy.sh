f24ae2477d186deae1c5c9de58e1eb3ddd278061
nathants <me@nathants.com>
2022-02-07 21:36:36 -1000

    NOJS should effect html gzipping

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,16 +12,16 @@ if [ -z "${NOJS:-}" ]; then
         rm -rf public/js/
         npx shadow-cljs release app
     ) 2>&1 | sed 's/^/shadow-cljs: /'
-fi
 
-# inline js into html then gzip
-temp=$(mktemp)
-cat frontend/public/index.html | grep script -B100 | grep -v script >> $temp
-echo '<script type="text/javascript" charset="utf-8">' >> $temp
-cat frontend/public/js/main.js >> $temp
-echo '</script>' >> $temp
-cat frontend/public/index.html | grep script -A100 | grep -v script >> $temp
-cat $temp | gzip --best > frontend/public/index.html.gzip
-rm $temp frontend/public/js/*
+    # inline js into html then gzip
+    temp=$(mktemp)
+    cat frontend/public/index.html | grep script -B100 | grep -v script >> $temp
+    echo '<script type="text/javascript" charset="utf-8">' >> $temp
+    cat frontend/public/js/main.js >> $temp
+    echo '</script>' >> $temp
+    cat frontend/public/index.html | grep script -A100 | grep -v script >> $temp
+    cat $temp | gzip --best > frontend/public/index.html.gzip
+    rm $temp frontend/public/js/*
+fi
 
 cli-aws lambda-ensure backend/*.go 2>&1 | sed 's/^/cli-aws: /'
