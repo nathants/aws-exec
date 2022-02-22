f4ad47756d5c451e2ef8aee343764d10f023b12b
nathants <me@nathants.com>
2022-02-21 21:14:15 -1000

    make exec useable as an api

--- cmd/exec/exec.go
+++ cmd/exec/exec.go
@@ -1,14 +1,9 @@
 package awsrce
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"time"
 
 	"github.com/alexflint/go-arg"
 	"github.com/nathants/aws-rce/rce"
@@ -31,111 +26,17 @@ func (execArgs) Description() string {
 func exec() {
 	var args execArgs
 	arg.MustParse(&args)
-
 	_ = os.Getenv("PROJECT_NAME")
 	domain := os.Getenv("PROJECT_DOMAIN")
 	auth := os.Getenv("AUTH")
 	url := fmt.Sprintf("https://%s", domain)
 	ctx := context.Background()
-
-	postResponse := rce.ExecPostResponse{}
-	err := lib.RetryAttempts(ctx, 7, func() error {
-		client := http.Client{}
-		data, err := json.Marshal(rce.ExecPostRequest{
-			Argv: args.Argv,
-		})
-		if err != nil {
-			return err
-		}
-		req, err := http.NewRequest(http.MethodPost, url+"/api/exec", bytes.NewReader(data))
-		req.Header.Set("auth", rce.Blake2b32(auth))
-		if err != nil {
-			return err
-		}
-		out, err := client.Do(req)
-		if err != nil {
-			lib.Logger.Println("error:", err)
-			return err
-		}
-		defer func() { _ = out.Body.Close() }()
-		data, err = ioutil.ReadAll(out.Body)
-		if err != nil {
-			return err
-		}
-		if out.StatusCode == 200 {
-			err = json.Unmarshal(data, &postResponse)
-			if err != nil {
-				return err
-			}
-			return nil
-		}
-		if fmt.Sprint(out.StatusCode)[:1] == "5" {
-			return fmt.Errorf("%d %s", out.StatusCode, string(data))
-		}
-		if out.StatusCode == 409 {
-			return fmt.Errorf("%d %s", out.StatusCode, string(data))
-		}
-		panic(fmt.Sprintf("%d %s", out.StatusCode, string(data)))
-	})
+	callback := func(logs string) {
+		fmt.Println(logs)
+	}
+	exitCode, err := rce.Exec(ctx, url, auth, args.Argv, callback)
 	if err != nil {
 		lib.Logger.Fatal("error: ", err)
 	}
-
-	increment := 0
-	for {
-		getResp := rce.ExecGetResponse{}
-		err := lib.RetryAttempts(ctx, 7, func() error {
-			client := http.Client{}
-			req, err := http.NewRequest(http.MethodGet, url+fmt.Sprintf("/api/exec?uid=%s&increment=%d", postResponse.Uid, increment), nil)
-			if err != nil {
-				return err
-			}
-			req.Header.Set("auth", rce.Blake2b32(auth))
-			out, err := client.Do(req)
-			if err != nil {
-				return err
-			}
-			defer func() { _ = out.Body.Close() }()
-			data, err := ioutil.ReadAll(out.Body)
-			if err != nil {
-				return err
-			}
-			if out.StatusCode != 200 && out.StatusCode != 409 {
-				return fmt.Errorf("%d %s\n%s", out.StatusCode, out.Request.URL, string(data))
-			}
-			err = json.Unmarshal(data, &getResp)
-			if err != nil {
-				return err
-			}
-			getResp.HttpCode = out.StatusCode
-			return nil
-		})
-		if err != nil {
-			lib.Logger.Fatal("error: ", err)
-		}
-		if getResp.HttpCode == 409 {
-			lib.Logger.Println("waiting", postResponse.Uid, time.Now())
-			time.Sleep(1 * time.Second)
-			continue
-		}
-		if getResp.ExitCode != nil {
-			os.Exit(*getResp.ExitCode)
-		}
-		if getResp.Increment != nil {
-			out, err := http.Get(getResp.LogUrl)
-			if err != nil {
-				panic(err)
-			}
-			data, err := ioutil.ReadAll(out.Body)
-			if err != nil {
-				panic(err)
-			}
-			_ = out.Body.Close()
-			fmt.Println(string(data))
-			increment = *getResp.Increment
-			continue
-		}
-		panic("unreachable\n" + lib.Pformat(getResp))
-	}
-
+	os.Exit(exitCode)
 }
--- rce/rce.go
+++ rce/rce.go
@@ -1,9 +1,17 @@
 package rce
 
 import (
+	"bytes"
+	"context"
 	"crypto/rand"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
 
+	"github.com/nathants/cli-aws/lib"
 	"golang.org/x/crypto/blake2b"
 )
 
@@ -60,4 +68,117 @@ func RandKey() string {
 		panic(err)
 	}
 	return hex.EncodeToString(val)
+}
+
+func Exec(ctx context.Context, url, auth string, argv []string, logDataCallback func(logs string)) (int, error) {
+	postResponse := ExecPostResponse{}
+	err := lib.RetryAttempts(ctx, 7, func() error {
+		client := http.Client{}
+		data, err := json.Marshal(ExecPostRequest{
+			Argv: argv,
+		})
+		if err != nil {
+			return err
+		}
+		req, err := http.NewRequest(http.MethodPost, url+"/api/exec", bytes.NewReader(data))
+		req.Header.Set("auth", Blake2b32(auth))
+		if err != nil {
+			return err
+		}
+		out, err := client.Do(req)
+		if err != nil {
+			lib.Logger.Println("error:", err)
+			return err
+		}
+		defer func() { _ = out.Body.Close() }()
+		data, err = ioutil.ReadAll(out.Body)
+		if err != nil {
+			return err
+		}
+		if out.StatusCode == 200 {
+			err = json.Unmarshal(data, &postResponse)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+		if fmt.Sprint(out.StatusCode)[:1] == "5" {
+			return fmt.Errorf("%d %s", out.StatusCode, string(data))
+		}
+		if out.StatusCode == 409 {
+			return fmt.Errorf("%d %s", out.StatusCode, string(data))
+		}
+		panic(fmt.Sprintf("%d %s", out.StatusCode, string(data)))
+	})
+	if err != nil {
+		lib.Logger.Println("error:", err)
+		return -1, err
+	}
+
+	increment := 0
+	for {
+		getResp := ExecGetResponse{}
+		err := lib.RetryAttempts(ctx, 7, func() error {
+			client := http.Client{}
+			req, err := http.NewRequest(http.MethodGet, url+fmt.Sprintf("/api/exec?uid=%s&increment=%d", postResponse.Uid, increment), nil)
+			if err != nil {
+				return err
+			}
+			req.Header.Set("auth", Blake2b32(auth))
+			out, err := client.Do(req)
+			if err != nil {
+				return err
+			}
+			defer func() { _ = out.Body.Close() }()
+			data, err := ioutil.ReadAll(out.Body)
+			if err != nil {
+				return err
+			}
+			if out.StatusCode != 200 && out.StatusCode != 409 {
+				return fmt.Errorf("%d %s\n%s", out.StatusCode, out.Request.URL, string(data))
+			}
+			err = json.Unmarshal(data, &getResp)
+			if err != nil {
+				return err
+			}
+			getResp.HttpCode = out.StatusCode
+			return nil
+		})
+		if err != nil {
+			lib.Logger.Println("error:", err)
+			return -1, err
+		}
+		if getResp.HttpCode == 409 {
+			lib.Logger.Println("waiting", postResponse.Uid, time.Now())
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		if getResp.ExitCode != nil {
+			return *getResp.ExitCode, nil
+		}
+		if getResp.Increment != nil {
+			var data []byte
+			err := lib.RetryAttempts(ctx, 7, func() error {
+				out, err := http.Get(getResp.LogUrl)
+				if err != nil {
+					return err
+				}
+				data, err = ioutil.ReadAll(out.Body)
+				if err != nil {
+					return err
+				}
+				_ = out.Body.Close()
+				return nil
+			})
+			if err != nil {
+				lib.Logger.Println("error:", err)
+				return -1, err
+			}
+			logDataCallback(string(data))
+			increment = *getResp.Increment
+			continue
+		}
+		panic("unreachable\n" + lib.Pformat(getResp))
+	}
+
 }
