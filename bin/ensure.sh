d4ef9be655a6fd76ce02ffbabd878d739c13d607
nathants <me@nathants.com>
2022-06-17 22:17:53 -1000

    improve js inlining

--- bin/ensure.sh
+++ bin/ensure.sh
@@ -3,6 +3,29 @@ set -eou pipefail
 
 source env.sh
 
+# inline js and add csp to the markup from frontend/public/index.html
+inline() {
+    path=$1
+    js=$(cat $path)
+    hash=$(echo -n "$js" | openssl sha256 -binary | openssl base64)
+    cat <<EOF
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="Content-Security-Policy" content="script-src 'sha256-${hash}'">
+      <link rel="icon" href="/favicon.png">
+      <link rel="apple-touch-icon" href="/favicon.png">
+      <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'/>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script type="text/javascript" charset="utf-8">${js}</script>
+    </body>
+</html>
+EOF
+}
+
 if [ -z "${NOJS:-}" ]; then
     (
         cd frontend
@@ -10,17 +33,8 @@ if [ -z "${NOJS:-}" ]; then
         rm -rf public/js/
         npx shadow-cljs release app
     ) 2>&1 | sed 's/^/shadow-cljs: /'
-
-    # inline js into html then gzip
-    temp=$(mktemp)
-    hash=$(cat frontend/public/js/main.js | openssl sha256 -binary | openssl base64)
-    cat frontend/public/index.html | grep "text/javascript" -B100 | grep -v "text/javascript" | sed "s:HASH:$hash:" >> $temp
-    echo -n '<script type="text/javascript" charset="utf-8">' >> $temp
-    cat frontend/public/js/main.js >> $temp
-    echo '</script>' >> $temp
-    cat frontend/public/index.html | grep "text/javascript" -A100 | grep -v "text/javascript" >> $temp
-    cat $temp | gzip --best > frontend/public/index.html.gz
-    rm $temp frontend/public/js/*
+    inline frontend/public/js/main.js | gzip --best > frontend/public/index.html.gz
+    rm -rf frontend/public/js/*
 fi
 
 libaws infra-ensure infra.yaml 2>&1 | sed 's/^/libaws: /'
