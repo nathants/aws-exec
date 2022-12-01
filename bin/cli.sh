ea75f8e07e7a9192eb54136ddec2b471fe3cecfc
nathants <me@nathants.com>
2022-11-30 21:33:29 -1000

    improve cli

--- bin/cli.sh
+++ bin/cli.sh
@@ -2,10 +2,13 @@
 source env.sh
 
 hash=$(find -name "*.go" -o -name "go.*" | sort | xargs cat | sha256sum | awk '{print $1}')
-path=/tmp/aws-exec.$hash
+path=/tmp/aws-exec-cli
+hash_path=${path}.hash
 
-if ! [ -f $path ]; then
+if [ ! -f $path ] || [ "$(cat $hash_path 2>/dev/null)" != "$hash" ]; then
+    echo rebuild $path $hash 1>&2
     go build -o $path .
+    echo -n "$hash" > $hash_path
 fi
 
 $path "$@"
