bcd6ded4f139002ebfd3f8159ef727f666272823
nathants <me@nathants.com>
2025-04-07 19:36:28 -1000

    pass env in dev.sh

--- bin/dev.sh
+++ bin/dev.sh
@@ -4,9 +4,9 @@ set -eou pipefail
 source ${1:-env.sh}
 
 # auto reload frontend
-bash bin/dev_frontend.sh 2>&1 | sed 's/^/shadow-cljs: /' &
+bash bin/dev_frontend.sh ${1:-env.sh} 2>&1 | sed 's/^/shadow-cljs: /' &
 pid=$!
 trap "kill $pid &>/dev/null || true" EXIT
 
 # auto reload backend
-find -name "*.go" -o -name "go.*" | entr -r bash bin/quick.sh 2>&1 | sed 's/^/libaws: /'
+find -name "*.go" -o -name "go.*" | entr -r bash bin/quick.sh ${1:-env.sh} 2>&1 | sed 's/^/libaws: /'
